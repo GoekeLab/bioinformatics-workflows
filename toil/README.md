561c4693344efdf60aba5efd119db5003c7c4324
lukas ho-koziol <lujho@ucsc.edu>
2021-09-03 11:02:22 -0700

    Adds to README, fix python pipeline

--- toil/README.md
+++ toil/README.md
@@ -1,34 +1,60 @@
 ## About Toil
-Add short description and link to official website
+Toil is a scalable, efficient, cross-platform pipleline management system written entirely in Python and designed around the principles of functional programming.
+https://toil.ucsc-cgl.org/
 
 ## Training material and documentation
-- Link to tutorial
-- Link to online documentation
-- any other helpful links
+https://toil.readthedocs.io/en/latest/
 
-## Community-developed workflows in X
+Tutorial for using Toil Python API for workflows 
+https://toil.readthedocs.io/en/latest/developingWorkflows/developing.html
+
+Tutorial for using Toil with WDL
+https://toil.readthedocs.io/en/latest/running/wdl.html
+
+Tutorial for using Toil with CWL
+https://toil.readthedocs.io/en/latest/running/cwl.html
+
+## Community-developed workflows in Toil
 Add description and links to community-developed pipelines.
 
-## Running the proof of concept X pipeline
+## Running the proof of concept Toil pipeline
+
+# General
+
+1. Install Toil by following the instructions listed [here](https://toil.readthedocs.io/en/latest/gettingStarted/install.html)
+
+2. (Optional) Install Docker by following the instructions listed [here](https://docs.docker.com/get-docker/)
+Docker isn't a necessity however we offer the option of using docker to increase the portability of the workflows
 
 # WDL
 
-1. Step 1
-2. Step 2
+1. Install the files under `toil/wdl` locally
+    - `example.wdl` is the workflow specification 
+    - `wdl_input.json` is the input data, change this file to point to desired paths
+2. Run the pipeline with the following command:
+    `toil-wdl-runner example.wdl wdl_input.json`
 
 # CWL
 
-1. Step 1
-2. Step 2
+1. Install the files under `toil/cwl` locally
+    - `example.cwl` is the workflow specification
+    - `fastqc.cwl` is a command line tool used by the workflow
+    - `salmonIndex.cwl` is a command line tool used by the workflow
+    - `salmonQuant.cwl` is a command line tool used by the workflow
+    - `cwl_input.yaml` is the input data, change this file to point to desired paths
+2. Run the pipeline with the following command:
+    `toil-cwl-runner example.cwl cwl_input.yaml`
 
 # Python
 
-1. Step 1
-2. Step 2
+1. Install the files under `toil/python` locally
+    - `example.py` is both workflow specification and input
+2. Run the pipeline with the following command:
+    `python example.py file:my-job-store`
 
 
 ## Notes and Contribution
-This pipeline is a minimal example of using X. We welcome contributions to the documentation and workflow, please create an issue or submit a pull request!
+This pipeline is a minimal example of using Toil. We welcome contributions to the documentation and workflow, please create an issue or submit a pull request!
 
 ## How to cite X
 
--- toil/python/example.py
+++ toil/python/example.py
@@ -4,53 +4,63 @@ import subprocess
 from toil.common import Toil
 from toil.job import Job
 from toil.lib.docker import apiDockerCall
-'''
-align = Job.wrapJobFn(apiDockerCall,
-                      image='ubuntu',
-                      working_dir=os.getcwd(),
-                      parameters=['ls', '-lha'])
-'''
-# def fastQC():
-#     subprocess.run(["fastqc", "data/reads_1.fq", "data/reads_2.fq"])
-    #os.system("fastqc data/reads_1.fq")
-#"/home/hexotical/bioinformatics-workflows/test_data/reads_1.fq.gz"
 
-fastQCone = Job.wrapJobFn(apiDockerCall,
+#Docker flag to change for using docker images
+DOCKER_FLAG = True
+
+#Necessary file paths
+fastQ_one = 'data/reads_1.fq'
+fastQ_two = 'data/reads_2.fq'
+transcript_fastA = 'data/transciptome.fa'
+
+
+fastQCDock = Job.wrapJobFn(apiDockerCall,
                         image='pegi3s/fastqc',
-                        working_dir='/home/hexotical/bioninformatics-workflows/toil/python',
-                        parameters=['fastqc', '-o', '/home/hexotical/bioninformatics-workflows/toil/python', 'data/reads_1.fq', 'data/reads_2.fq'])
+                        working_dir= os.getcwd(),
+                        parameters=['fastqc', fastQ_one, fastQ_two])
 
-salmonIndex = Job.wrapJobFn(apiDockerCall,
+salmonIndexDock = Job.wrapJobFn(apiDockerCall,
                         image='combinelab/salmon',
                         working_dir='/home/hexotical/bioninformatics-workflows/toil/python',
-                        parameters=['salmon', 'index', '-t', '/home/hexotical/bioinformatics-workflows/toil/python/data/transcriptome.fa', '-i', 'index'])
+                        parameters=['salmon', 'index', '-t', transcript_fastA, '-i', 'index'])
 
-# def salmonIndex():
-#     subprocess.run(["salmon", "index", "-t", "data/transcriptome.fa"])
-    #subprocess.run(['tar', '-cvzf', 'index.tar.gz', 'index'])
-#almon index -t "${ref_txome}" -i ${index}; tar -cvzf index.tar.gz ${index}
-#/home/hexotical/bioinformatics-workflows/toil/python/data/transcriptome.fa
-#salmonAlignQuaint = Job.wrapJobFn
-# def salmonAlignQuaint():
-#     subprocess.run(["salmon", "quant", "-i", "index", "-l", "A", "-1", "data/reads_1.fq", "-2", "data/reads_2.fq", "--validateMappings", "-o", "quant"])
-#salmon quant -i "${indexdir}" -l A -1 "${reads1}" -2 "${reads2}" --validateMappings -o quant;
-#AddChildren
-salmonQuant = Job.wrapJobFn(apiDockerCall,
+salmonQuantDock = Job.wrapJobFn(apiDockerCall,
                         image='combinelab/salmon',
                         working_dir='/home/hexotical/bioninformatics-workflows/toil/python',
-                        parameters=['salmon', 'quant', '-i', 'index', '-l', 'A', '-1', 'data/reads_1.fq', '-2', 'data/reads_2.fq', '--validateMappings', '-o', 'quant'])
+                        parameters=['salmon', 'quant', '-i', 'index', '-l', 'A', '-1', fastQ_one, '-2', fastQ_two, '--validateMappings', '-o', 'quant'])
+
+def fastQC(self):
+    subprocess.run(["fastqc", fastQ_one, fastQ_two])
+
+
+def salmonIndex(self):
+    subprocess.run(["salmon", "index", "-t", transcript_fastA, '-i', 'index'])
+
+def salmonAlignQuant(self):
+    subprocess.run(["salmon", "quant", "-i", "index", "-l", "A", "-1", fastQ_one, "-2", fastQ_two, "--validateMappings", "-o", "quant"])
+
+fastQ = Job.wrapJobFn(fastQC)
+salmonI = Job.wrapJobFn(salmonIndex)
+salmonQ = Job.wrapJobFn(salmonAlignQuant)
+
+
 
 if __name__=="__main__":
     options = Job.Runner.getDefaultOptions("./toilWorkflowRun")
     options.logLevel = "DEBUG"
     options.clean = "always"
 
-    salmonIndex.addChild(salmonQuant) # We add a child to ensure that the necessary files exist
+
+    # We add a child to ensure that the necessary files exist by the time Toil attempts to run the job
+    salmonIndexDock.addChild(salmonQuantDock)
+    salmonI.addChild(salmonQ)
 
     with Toil(options) as toil:
-        toil.start(fastQCone)
-        toil.start(salmonIndex)
-        toil.start(salmonQuant)
-       #toil.start(Job.wrapFn(fastQC))
-       #toil.start(Job.wrapFn(salmonIndex))
-       #toil.start(Job.wrapFn(salmonAlignQuaint))
+        if(DOCKER_FLAG):
+            toil.start(fastQCDock)
+            toil.start(salmonIndexDock)
+            toil.start(salmonQuantDock)
+        else:
+            toil.start(fastQ)
+            toil.start(salmonI)
+            toil.start(salmonQ)
